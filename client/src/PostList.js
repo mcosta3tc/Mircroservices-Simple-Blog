bb10a29b9e49ff929ee260ca30b1d9b8c2c7b927
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-11-11 18:29:35 +0100

    Fetching posts

---
+++ client/src/PostList.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
+export default () => {
+    /**
+     * {} in useState => post MS return an {} in the res.send
+     */
+    const [posts, setPosts] = useState({});
+
+
+    const fetchPosts = async () => {
+        /**
+         * axios resp => data{}
+         */
+        const res =  await axios.get('http://localhost:4000/posts');
+
+        setPosts(res.data);
+    }
+
+    /**
+     * triggered at the launch of the component
+     * [] => run this fn 1x
+     */
+    useEffect(()=>{
+        fetchPosts()
+    }, [])
+
+    /**
+     * Object.values = [] of all posts
+     * map [posts] && foreach post => generate jsx
+     */
+    const renderedPost = Object.values(posts).map(post => {
+        return (
+                /**
+                 * key = {} :
+                 *  - posts => generated list of elements
+                 *  - react expect a unique key property on each element created
+                 *  - this key => id of the post
+                 */
+                <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
+                    <div className='card-body'>
+                        <h3>{post.title}</h3>
+                    </div>
+                </div>
+        )
+    })
+
+    return(
+            <div className='d-flex flex-row flex-wrap justify-content-between'>
+                {renderedPost}
+            </div>
+    )
+}
