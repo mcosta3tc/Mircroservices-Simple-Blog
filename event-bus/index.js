4f3095fbffe0e244e9b39691c49abd168a14aedc
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-11-12 12:02:45 +0100

    event bus init

---
+++ event-bus/index.js
@@ -0,0 +1,37 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+/**
+ * Post handler => foreach event:
+ *  - request to the different services servers
+ */
+app.post('/events', ((req, res) => {
+    /**
+     * event = the body of request, of any kind (json, string, obj...)
+     * event => Services
+     */
+    const events = req.body;
+
+    //Posts
+    axios.post('http://localhost:4000/events', events);
+
+    //Comments
+    axios.post('http://localhost:4001/events', events);
+
+    //
+    axios.post('http://localhost:4002/events', events);
+
+    /**
+     * foreach committed event send ok => worked
+     */
+    res.send({status:'OK'});
+}));
+
+app.listen(4005, ()=>{
+    console.log('listening 4005');
+})
+
