dc91b80bcd5fd3ac11f7feebd07e79d2d3be933f
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-11-16 17:28:46 +0100

    front updated to moderate comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -7,23 +7,44 @@ export default ({comments}) => {
      * map [comments] && foreach comment => generate jsx
      */
     const renderedComments = comments.map(comment => {
+        /**
+         * check comment status
+         *  - approved
+         *  - rejected
+         *  default : pending
+         *  => change the content of the comment
+         */
+        let content;
+
+        switch(comment.status) {
+            case 'approved' :
+                content = comment.content;
+                break;
+            case 'rejected' :
+                content = 'this comment has been rejected';
+                break;
+            default:
+                content = 'this comment is awaiting moderation';
+        }
+
         return (
                 /**
                  * key = {} :
                  *  - comments => generated list of elements
                  *  - react expect a unique key property on each element created
                  *  - this key => id of the comment
+                 *  - show verified content
                  */
-                <li key={comment.commentId}>{comment.content}</li>
-        )
-    })
+                <li key={comment.commentId}>{content}</li>
+        );
+    });
 
     /**
      * rendering the comments
      */
     return (
-           <ul>
-               {renderedComments}
-           </ul>
-    )
+            <ul>
+                {renderedComments}
+            </ul>
+    );
 }
